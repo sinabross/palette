b11eee87637c139f0087193c0b22c03f8d2a8a6f
Seongho Song <sshthelegend@gmail.com>
2016-08-08 08:10:38 +0000

    front commit(2)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,6 @@
 class HomeController < ApplicationController
   def index
-     #code for searching lip products
-      @lips = Lipdb.all
-    if params[:search]
-      @lips = Lipdb.search(params[:search]).order("created_at DESC")
-    else
-      @lips = Lipdb.all.order('created_at DESC')
-    end
-     #code for serching eye shadow products
-       @eyes = Eyedb.all
-    if params[:search]
-      @eyes = Eyedb.search(params[:search]).order("created_at DESC")
-    else
-      @eyes = Eyedb.all.order('created_at DESC')
-    end
+    
   end
   
   def layout4
