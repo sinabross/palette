edd1929129b31e2aadb65dffc9aed6f1b5c447c2
Seongho Song <sshthelegend@gmail.com>
2016-08-08 13:50:45 +0000

    search bar added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'home/layout5'
   get 'home/layout4'
   
+ 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
