ebdf3bed6bd534c4d44de9925e71c7eedf17a1c3
Seongho Song <sshthelegend@gmail.com>
2016-08-18 11:45:18 +0000

    검색결과에서 페이지번호 추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,15 +11,15 @@ class HomeController < ApplicationController
        @search3 = params[:search].split(" ").third
         
        if params[:search]
-         @result = Lipdb.all.search(@search,@search2,@search3).order("id DESC")
+         @result = Lipdb.all.search(@search,@search2,@search3).order("id DESC").paginate(page: params[:page], per_page: 21)
        else
-         @result = Lipdb.all.order("zzim desc")
+         @result = Lipdb.all.order("zzim desc").paginate(page: params[:page], per_page: 21)
        end
      # 섀도우 제품 검색결과  
        if params[:search]
-         @result2 = Eyedb.all.search(@search,@search2,@search3).order("id DESC")
+         @result2 = Eyedb.all.search(@search,@search2,@search3).order("id DESC").paginate(page: params[:page], per_page: 21)
        else
-         @result2 = Eyedb.all.order("zzim desc")
+         @result2 = Eyedb.all.order("zzim desc").paginate(page: params[:page], per_page: 21)
        end
   end
   
