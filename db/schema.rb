1c3f03bf23023388bfa01a9d935b69186214cff3
youngjin choi <yj90401@gmail.com>
2016-08-26 10:58:44 +0000

    layout4, 5, search

--- db/schema.rb
+++ db/schema.rb
@@ -49,13 +49,6 @@ ActiveRecord::Schema.define(version: 20160825080043) do
     t.datetime "updated_at",              null: false
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.integer  "review_id"
-    t.string   "light"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reviews", force: :cascade do |t|
     t.string   "num"
     t.string   "content"
