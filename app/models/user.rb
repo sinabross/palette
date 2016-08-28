13a978560d9ec5bbf1cec25aafa5a6965d111052
Seongho Song <sshthelegend@gmail.com>
2016-08-28 05:23:28 +0000

    profile edit added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :reviews, dependent: :destroy #dependent는 user가 탈퇴하면 그 사람이 쓴 리뷰들 자동으로 삭제하게 하는 코드
+  has_many :reviews #, dependent: :destroy #dependent는 user가 탈퇴하면 그 사람이 쓴 리뷰들 자동으로 삭제하게 하는 코드
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable
          
