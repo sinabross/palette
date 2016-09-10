d746a6176ef87f62f475ca6748e0432a0c1236c8
Seongho Song <sshthelegend@gmail.com>
2016-09-10 12:43:46 +0000

    production rb 와 assets.rb 수정

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-  Rails.application.config.assets.precompile += ['bootstrap.js', 'bootstrap.min.js', 'contact_me.js', 'application.js', 'application.css', 'jquery.js', 'bootstrap.min.css', 'bootstrap.css', 'modern-business.css']
+# Rails.application.config.assets.precompile += ['bootstrap.js', 'bootstrap.min.js', 'contact_me.js', 'application.js', 'application.css', 'jquery.js', 'bootstrap.min.css', 'bootstrap.css', 'modern-business.css']
