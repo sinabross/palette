4f3b50e574d51082696a4215901412c89a2663ce
Garsong <sshthelegend@gmail.com>
2016-10-03 17:46:43 +0900

    글 작성 완료 메시지 안보이는거 수정

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,8 @@ class ApplicationController < ActionController::Base
   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :username, :userseason) }
   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit({ roles: [] }, :username, :password, :password_confirmation, :current_password) }
   end
-  
+
+
   
   
 end
