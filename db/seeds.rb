f7573f9a859477909d0b915d93c7cbcdbbc38cf1
Garsong <sshthelegend@gmail.com>
2016-10-15 20:56:50 +0900

    공지사항 조회수 체크

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-
-
     xlsx_lip = Roo::Excelx.new("xlsx/lipdb.xlsx")
     input_lip= xlsx_lip.parse(  num: 'num',
                                 wc: 'wc',
                                 season: 'season',
-                                tone: 'tone', 
+                                tone: 'tone',
                                 brand: 'brand',
                                 name: 'name',
                                 price: 'price',
@@ -27,25 +25,25 @@
         Lipdb.create(item)
     end
                          
-    xlsx_eye = Roo::Excelx.new("xlsx/eyedb.xlsx")
-    input_eye= xlsx_eye.parse(  num: 'num',
-                                wc: 'wc',
-                                season: 'season',
-                                tone: 'tone', 
-                                brand: 'brand',
-                                name: 'name',
-                                price: 'price',
-                                size: 'size',
-                                zzim: 'zzim',
-                                pro_type: 'pro_type',
-                                glitter: 'glitter')
+   # xlsx_eye = Roo::Excelx.new("xlsx/eyedb.xlsx")
+    #input_eye= xlsx_eye.parse(  num: 'num',
+     #                           wc: 'wc',
+      #                          season: 'season',
+       #                         tone: 'tone',
+        #                        brand: 'brand',
+         #                       name: 'name',
+          #                      price: 'price',
+           #                     size: 'size',
+            #                    zzim: 'zzim',
+             #                   pro_type: 'pro_type',
+              #                  glitter: 'glitter')
     
     
     
-    input_eye.each_with_index do |item, index|
-        next if index == 0
-        Eyedb.create(item)
-    end
+  #  input_eye.each_with_index do |item, index|
+   #     next if index == 0
+    #    Eyedb.create(item)
+    #end
     
     
    # User.create(username: '테스트유저', userseason: '봄', email: 'user@gmail.com', password: 'useruser')
