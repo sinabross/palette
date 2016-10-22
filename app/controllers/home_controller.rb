2af18ebc60b030f5b8e36ab000fb2f84e366e6d5
Garsong <sshthelegend@gmail.com>
2016-10-22 15:19:58 +0900

    빵부스러기목록 추가 및 검토요청 내용 수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -68,55 +68,56 @@ class HomeController < ApplicationController
       #웜립일때
       @colors = "w_lips"
       @color_tone = "웜"      
-      @liplist = params[:liplist]
-      @liptone = params[:liptone]
+     # @liplist = params[:liplist]
+      @liptone ||= params[:liptone]
       #웝립/제품군/세부톤
       @list = Lipdb.get_w_lip.where(tone:params[:liptone]).order('brand').paginate(page: params[:page], per_page: 21)
-    if params[:liplist] == nil
-       @liplist = "제품"
-       @list = Lipdb.get_w_lip.where(tone:params[:liptone]).order('brand').paginate(page: params[:page], per_page: 21)
-        if params[:liptone] == nil
-          @liptone = "세부톤"
+   # if params[:liplist] == nil
+    #   @liplist = "제품"
+     #  @list = Lipdb.get_w_lip.where(tone:params[:liptone]).order('brand').paginate(page: params[:page], per_page: 21)
+      if params[:liptone] == nil
+          @liptone = "세부톤 전체보기"
           @list = Lipdb.get_w_lip.order('brand').paginate(page: params[:page], per_page: 21)
-        end
-    end
-      #세부톤 선택안했을때
-     if params[:liptone] == nil
-        @liptone = "세부톤"
-        #웜립/제품군에 따라 출력
-        @list = Lipdb.get_w_lip.where(pro_type:params[:liplist]).order('brand').paginate(page: params[:page], per_page: 21)
-         #세부톤,제품군 선택안했을때 웜립 전체출력
-        if params[:liplist] == nil 
-          @liplist = "제품"
-          @list = Lipdb.get_w_lip.order('brand').paginate(page: params[:page], per_page: 21)
-        end
       end
-    end
+     end
+      #세부톤 선택안했을때
+  #   if params[:liptone] == nil
+  #      @liptone = "세부톤"
+        #웜립/제품군에 따라 출력
+  #      @list = Lipdb.get_w_lip.where(pro_type:params[:liplist]).order('brand').paginate(page: params[:page], per_page: 21)
+         #세부톤,제품군 선택안했을때 웜립 전체출력
+  #      if params[:liplist] == nil
+  #        @liplist = "제품"
+  #        @list = Lipdb.get_w_lip.order('brand').paginate(page: params[:page], per_page: 21)
+  #      end
+  #    end
+  #  end
 
     # 쿨톤 립 출력
     if params[:colors] == "c_lips"
       @colors = "c_lips"
       @color_tone = "쿨"      
-      @liplist = params[:liplist]
+    #  @liplist = params[:liplist]
       @liptone = params[:liptone]
       @list = Lipdb.get_c_lip.where(tone:params[:liptone]).order('brand').paginate(page: params[:page], per_page: 21)
-      if params[:liplist] == nil
-        @liplist = "제품"
-        @list = Lipdb.get_c_lip.where(tone:params[:liptone]).order('brand').paginate(page: params[:page], per_page: 21)
+    #  if params[:liplist] == nil
+    #    @liplist = "제품"
+    #    @list = Lipdb.get_c_lip.where(tone:params[:liptone]).order('brand').paginate(page: params[:page], per_page: 21)
         if params[:liptone] == nil
-          @liptone = "세부톤"
+          @liptone = "세부톤 전체보기"
           @list = Lipdb.get_c_lip.order('brand').paginate(page: params[:page], per_page: 21)
         end
-      end
-      if params[:liptone] == nil
-        @liptone = "세부톤"
-        @list = Lipdb.get_c_lip.where(pro_type:params[:liplist]).order('brand').paginate(page: params[:page], per_page: 21)
-        if params[:liplist] == nil 
-          @liplist = "제품"
-          @list = Lipdb.get_c_lip.order('brand').paginate(page: params[:page], per_page: 21)
-        end
-      end
     end
+
+   #   if params[:liptone] == nil
+   #     @liptone = "세부톤"
+   #     @list = Lipdb.get_c_lip.where(pro_type:params[:liplist]).order('brand').paginate(page: params[:page], per_page: 21)
+   #     if params[:liplist] == nil
+   #       @liplist = "제품"
+   #       @list = Lipdb.get_c_lip.order('brand').paginate(page: params[:page], per_page: 21)
+   #     end
+   #   end
+   # end
   end
 
 
@@ -187,6 +188,10 @@ class HomeController < ApplicationController
     
     # 리뷰 작성 시에 해당 제품의 리뷰 페이지로 연결될 수 있도록 변수 지정
     @review=Review.where(num:params[:product_num])
+
+    #for page-navigation
+
+
   end
   
   #================= 아래는 좋아요 기능을 위한 노력의 흔적들..........
