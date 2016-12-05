25f73676c09cdb2d4ff2c192ee1b5df46dd1cb9d
Garsong <sshthelegend@gmail.com>
2016-12-05 14:35:35 +0900

    필터 버튼 디자인 수정

---
+++ db/migrate/20161205052640_create_lipdb.rb
@@ -0,0 +1,35 @@
+class CreateLipdb < ActiveRecord::Migration
+  def change
+    create_table :lipdbs do |t|
+
+      t.string  :num
+      t.string  :image
+      t.string  :color_PA
+      t.string  :season_PA
+      t.string  :tone_PA
+      t.string  :color_BY
+      t.string  :season_BY
+      t.string  :tone_BY
+      t.string  :color_YS
+      t.string  :season_YS
+      t.string  :tone_YS
+      t.string  :color_total
+      t.string  :season_total
+      t.string  :tone_total
+      t.string  :comment_PA
+      t.string  :comment_BY
+      t.string  :comment_YS
+      t.string  :brand
+      t.string  :name
+      t.integer :price
+      t.string  :size
+      t.integer :zzim, default:"0"
+      t.string  :pro_type
+      t.string  :glitter
+      t.string  :texture
+      t.string  :keyword
+
+      t.timestamps null: false
+    end
+  end
+end
