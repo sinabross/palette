0c89b26f5dfaddf214f5a5a108f6850389a6369a
Garsong <sshthelegend@gmail.com>
2016-12-27 18:52:38 +0900

    제품요청 리스트 들어온 것 볼 수 있게 추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -418,15 +418,17 @@ class HomeController < ApplicationController
   #=============== admin page============================
   
   def admin_reply #admin 댓글모아보기
-    unless user_signed_in?
-      redirect_to "/users/sign_in"
-    end
-    if user_signed_in? && current_user.admin?
-      @review= Review.all.order("created_at DESC")
-    elsif user_signed_in? && current_user.admin==nil
-      redirect_to "/"
-      flash[:error] = "접근권한이 없습니다."
-    end
+  #  unless user_signed_in?
+  #    redirect_to "/users/sign_in"
+  #  end
+  #  if user_signed_in? && current_user.admin?
+  #    @review= Review.all.order("created_at DESC")
+  #  elsif user_signed_in? && current_user.admin==nil
+  #     redirect_to "/"
+  #    flash[:error] = "접근권한이 없습니다."
+  #  end
+    @asks = Askfor.order('id desc').paginate(page: params[:page], per_page: 5)
+
   end
   
   def admin_user
