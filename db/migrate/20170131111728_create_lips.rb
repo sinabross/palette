67363e3afd85adfa4a58ca1ed4da88dd339fccb1
Garsong <sshthelegend@gmail.com>
2017-01-31 20:24:51 +0900

    table 재생성 및 시드 데이터 삽입

---
+++ db/migrate/20170131111728_create_lips.rb
@@ -0,0 +1,24 @@
+class CreateLips < ActiveRecord::Migration
+  def change
+    create_table :lips do |t|
+
+      t.string  :num
+      t.string  :image
+      t.string  :name
+      t.string  :brand
+      t.string  :level
+      t.string  :color
+      t.string  :wc
+      t.string  :tone
+      t.string  :season
+      t.string  :price
+      t.string  :size
+      t.string  :pro_type
+      t.string  :glitter
+      t.string  :texture
+      t.string  :keyword
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131110609) do
+ActiveRecord::Schema.define(version: 20170131111728) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -118,6 +118,26 @@ ActiveRecord::Schema.define(version: 20170131110609) do
     t.string   "keyword"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "lips", force: :cascade do |t|
+    t.string   "num"
+    t.string   "image"
+    t.string   "name"
+    t.string   "brand"
+    t.string   "level"
+    t.string   "color"
+    t.string   "wc"
+    t.string   "tone"
+    t.string   "season"
+    t.string   "price"
+    t.string   "size"
+    t.string   "pro_type"
+    t.string   "glitter"
+    t.string   "texture"
+    t.string   "keyword"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notices", force: :cascade do |t|
