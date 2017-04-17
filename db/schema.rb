4c1f435a88f1d6027fd95b0e34ad147b69b602a1
Yerak Kang <ediya7@gmail.com>
2017-04-17 10:24:14 +0000

    DB 다시 업데이트

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417093805) do
+ActiveRecord::Schema.define(version: 20170417102212) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
