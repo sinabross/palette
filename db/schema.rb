84277dfcf2f71740d29a0dc65bc05b65a284e800
Yerak Kang <ediya7@gmail.com>
2017-05-08 11:44:19 +0000

    DB 업데이트, 리스트페이지에서 업데이트 스티커 추가

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504140124) do
+ActiveRecord::Schema.define(version: 20170508113738) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20170504140124) do
     t.string   "glitter"
     t.string   "texture"
     t.string   "keyword"
+    t.string   "update_date"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.integer  "cached_votes_total",      default: 0
