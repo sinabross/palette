4aa814a6473e0e248bf2d8498b94c77806ad20cd
Yerak Kang <ediya7@gmail.com>
2017-05-14 05:42:50 +0000

    제품 가격정보 오류 수정

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511111043) do
+ActiveRecord::Schema.define(version: 20170514053954) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
