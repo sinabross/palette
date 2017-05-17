bd31fedec4e3030bdf5b0e4db7ab65871759b03e
Yerak Kang <ediya7@gmail.com>
2017-05-17 00:56:12 +0000

    새소식 상단 고정 기능 구현, 푸터 간격 조정, 찜하기 업데이트 스티커 제거, 리스트페이지 가격 정보 위치 조정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -879,6 +879,7 @@ class HomeController < ApplicationController
     @edit.title = params[:title]
     @edit.name = params[:name]
     @edit.content = params[:content]
+    @edit.special = params[:special]
     @edit.save
 
     redirect_to '/home/notice'
@@ -928,6 +929,7 @@ class HomeController < ApplicationController
     @notices.title = params[:title]
     @notices.content = params[:content]
     @notices.name = params[:name]
+    @notices.special = params[:special]
     @notices.save
 
 
